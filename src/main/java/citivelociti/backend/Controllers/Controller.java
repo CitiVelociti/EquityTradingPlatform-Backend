c41035494f949ed2dcd58074c492bac4a1ceb943
Ben Zhuo <Benzhuo97@gmail.com>
2019-07-25 20:36:52 +0000

    Basic controller setup, port listening on 8081

---
+++ src/main/java/citivelociti/backend/Controllers/Controller.java
@@ -0,0 +1,14 @@
+package citivelociti.backend.Controllers;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String helloWorld(){
+        return "helloWorld";
+    }
+}
