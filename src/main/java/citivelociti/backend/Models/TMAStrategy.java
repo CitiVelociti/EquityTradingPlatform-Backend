9bc4fd8bb6b59ce9b2c546161a5e4c00666594bb
Ben <benzhuo97@gmail.com>
2019-07-29 12:05:33 -0400

    sql build fail

--- src/main/java/citivelociti/backend/Models/TMAStrategy.java
+++ src/main/java/citivelociti/backend/Models/TMAStrategy.java
@@ -1,18 +1,47 @@
-package citivelociti.backend.Models;
+/*package citivelociti.backend.Models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
 
 @Entity
 public class TMAStrategy extends Strategy {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name="id")
+    private Integer id;
 
     private Integer slowAvgIntervale;
     private Integer fastAvgIntervale;
-    private boolean shortBelow;
+    private Boolean shortBelow;
 
     public TMAStrategy(String ticker, Double volume, Double limit, Double stop, Integer slowAvgIntervale, Integer fastAvgIntervale){
         super(ticker, volume, limit, stop);
         this.slowAvgIntervale = slowAvgIntervale;
         this.fastAvgIntervale = fastAvgIntervale;
-    };
+    }
+
+    public Integer getSlowAvgIntervale() {
+        return slowAvgIntervale;
+    }
+
+    public void setSlowAvgIntervale(Integer slowAvgIntervale) {
+        this.slowAvgIntervale = slowAvgIntervale;
+    }
+
+    public Integer getFastAvgIntervale() {
+        return fastAvgIntervale;
+    }
+
+    public void setFastAvgIntervale(Integer fastAvgIntervale) {
+        this.fastAvgIntervale = fastAvgIntervale;
+    }
+
+    public Boolean getShortBelow() {
+        return shortBelow;
+    }
+
+    public void setShortBelow(Boolean shortBelow) {
+        this.shortBelow = shortBelow;
+    }
 }
+*/
