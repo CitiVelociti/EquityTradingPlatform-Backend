8f92d147a1fdd1721605867e0f805b510b9953aa
Ben <benzhuo97@gmail.com>
2019-08-02 16:53:34 -0400

    delete extra code

--- src/main/java/citivelociti/backend/Services/EngineService.java
+++ src/main/java/citivelociti/backend/Services/EngineService.java
@@ -144,19 +144,7 @@ public class EngineService {
         int n = arr.length;
         return Math.sqrt(variance(arr, n));
     }
-    public static double calculateSD(double numArray[])
-    {
-        double sum = 0.0, standardDeviation = 0.0;
-        int length = numArray.length;
-        for(double num : numArray) {
-            sum += num;
-        }
-        double mean = sum/length;
-        for(double num: numArray) {
-            standardDeviation += Math.pow(num - mean, 2);
-        }
-        return Math.sqrt(standardDeviation/length);
-    }
+    
     public void doTMACalculation(TMAStrategy tmaStrategy){
         String ticker = tmaStrategy.getTicker();
         Strategy bufferStrategy;
