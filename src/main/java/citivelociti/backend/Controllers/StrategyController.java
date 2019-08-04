2eb9a065163f628736d25159bb1f4db1878e4503
Fanny Dai <fannydai@gmail.com>
2019-08-04 15:15:48 -0400

    fixed bugs in strategy controller

--- src/main/java/citivelociti/backend/Controllers/StrategyController.java
+++ src/main/java/citivelociti/backend/Controllers/StrategyController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Logger;
 
 @RestController
 @CrossOrigin(origins = "*")
